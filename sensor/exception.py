463d02966e58672d4d1994debd074d9dde9e9cce
monu471 <monujoshi471@gmail.com>
2023-02-23 19:46:56 +0530

    pipeline completed

---
+++ sensor/exception.py
@@ -0,0 +1,19 @@
+import os,sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,ex_tb = error_detail.exc_info()
+    file_name = ex_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured python script name[{0}] line number [{1}] error_message [{2}]".format(file_name,ex_tb.tb_lineno,str(error))
+    return error_message
+
+
+
+
+class SensorException(Exception):
+
+    def __init__(self,error_message, error_detail:sys):
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
